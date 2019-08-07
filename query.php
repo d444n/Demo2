c703a5984dc0a7e756fe47c089dfa24be9038b0e
d444n <53837142+d444n@users.noreply.github.com>
2019-08-07 22:55:05 +0200

    Add files via upload

--- query.php
+++ query.php
@@ -2,7 +2,13 @@
 
 $query = "SELECT * FROM blog";
 
+$posts = $db->query($query);
 
 
-
+<?php if($posts->num_rows > 0) {
+    while($row = $posts->fetch_assoc()) {
 ?>
+
+<a href='#'><?php echo$row['title']; ?></a>
+
+<?php } } ?>
